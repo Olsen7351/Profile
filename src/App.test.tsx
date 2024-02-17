c31b90352dc092ddc8bef870419e63865eaf15d2
Olsen7351 <olsen7351@gmail.com>
2024-02-17 15:48:41 +0100

    Fix: Fixed example test so it works

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Home from "./Pages/Home";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("renders learn react link", () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Welcome to my hobby CV!/i);
+  expect(linkElement).toBeInTheDocument();
+});
